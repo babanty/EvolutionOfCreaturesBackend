0133ebf152e2f13cf724fa21fb7adb5e8407af52
Alexander Tkachuk <tkachuk.aleksandr.sergeevich@gmail.com>
2021-09-03 16:30:40 +0300

    added test: CreatePlayer__ValidCreateRequest__ResultOk

---
+++ EvolutionOfCreatures.Logic.Tests/Players/PlayerManager__CreateEntity.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EvolutionOfCreatures.Logic.Players;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using EvolutionOfCreatures.Db;
+using EvolutionOfCreatures.Logic.Tests.TestTools;
+
+namespace EvolutionOfCreatures.Logic.Players.Tests
+{
+    [TestClass()]
+    public class PlayerManager__CreateEntity
+    {
+        [TestMethod()]
+        public async Task CreateEntity__ValidCreateRequest__ResultOk()
+        {
+            // Arrange
+            var accountId = Guid.NewGuid();
+            var playerName = "stub";
+            var createPlayerRequest = new CreatePlayerRequest() { AccountId = accountId, PlayerName = playerName };
+            using var msSqlDbWrapper = GetNewMsSqlDbWrapper();
+            var entityManager = GetNewPlayerManager(msSqlDbWrapper.Instance);
+
+
+            // Act
+            var newEntity = await entityManager.CreateEntity(createPlayerRequest);
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, newEntity.Id);
+            Assert.AreNotEqual(default, newEntity.CreatedAt);
+            Assert.IsNotNull(newEntity.PlayerProgress);
+            Assert.IsNotNull(newEntity.PlayerSettings);
+            Assert.IsNotNull(newEntity.PlayerStatistics);
+            Assert.AreEqual(accountId, newEntity.AccountId);
+            Assert.AreEqual(playerName, newEntity.Name);
+            Assert.AreNotEqual(default, newEntity.Rating); // business case. Rating cannot be empty, because other players need to take the rating when they win.
+        }
+
+
+
+        private PlayerManager GetNewPlayerManager(EvolutionOfCreaturesContext db) => new PlayerManager(db, new CreatePlayerRequestValidator());
+
+
+        private MsSqlDbContextWrapper<EvolutionOfCreaturesContext> GetNewMsSqlDbWrapper()
+        {
+            var dbContext = DbContextUtilities.GetMsSqlTestDb<EvolutionOfCreaturesContext>();
+
+            return new MsSqlDbContextWrapper<EvolutionOfCreaturesContext>() { Instance = dbContext };
+        }
+    }
+}
--- EvolutionOfCreatures.Logic.Tests/TestTools/DbContextUtilities.cs
+++ EvolutionOfCreatures.Logic.Tests/TestTools/DbContextUtilities.cs
@@ -1,13 +1,76 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Data.SqlClient;
+using Infrastructure.Tools.Db;
+using System.Data;
+using Dapper;
 
 namespace EvolutionOfCreatures.Logic.Tests.TestTools
 {
-    public static class DbContextUtilities
+    internal static class DbContextUtilities
     {
-        public static DbContextOptions<TDbContext> GetInMemoryDbOptions<TDbContext>() where TDbContext : DbContext
+        public static TDbContext GetEfCoreInMemoryDb<TDbContext>() where TDbContext : DbContext => 
+                                            GetDbContextInstance(GetEfCoreInMemoryDbOptions<TDbContext>());
+
+
+        public static TDbContext GetSqlLiteInMemoryDb<TDbContext>() where TDbContext : DbContext =>
+                                            GetDbContextInstance(GetSqlLiteInMemoryDbOptions<TDbContext>(), true);
+
+        public static TDbContext GetMsSqlTestDb<TDbContext>() where TDbContext : DbContext =>
+                                            GetDbContextInstance(GetMsSqlTestDbOptions<TDbContext>(), true);
+
+
+        public static void DisposeAndDeleteMsSqlDb<TDbContext>(this TDbContext dbContext) where TDbContext : DbContext
+        {
+            var connectionString = dbContext.Database.GetConnectionString();
+
+            dbContext.Dispose();
+
+            MsSqlDbManager.DeleteDb(connectionString);
+        }
+
+
+        private static TDbContext GetDbContextInstance<TDbContext>(DbContextOptions<TDbContext> dbContextOptions, bool withMigrations = false) 
+                 where TDbContext : DbContext
+        {
+            var context = Activator.CreateInstance(typeof(TDbContext), dbContextOptions) as TDbContext;
+
+            if(withMigrations)
+                context.Database.Migrate();
+
+            return context;
+        }
+
+
+        private static DbContextOptions<TDbContext> GetMsSqlTestDbOptions<TDbContext>() where TDbContext : DbContext
+        {
+            var builder = new DbContextOptionsBuilder<TDbContext>();
+            var connectionString = $"Server=localhost;Database={nameof(TDbContext)}_Test;Trusted_Connection=True;MultipleActiveResultSets=true;Connect Timeout=15;Encrypt=False;Packet Size=4096";
+
+            builder.UseSqlServer(connectionString);
+            return builder.Options;
+        }
+
+
+        private static DbContextOptions<TDbContext> GetEfCoreInMemoryDbOptions<TDbContext>() where TDbContext : DbContext
         {
             var builder = new DbContextOptionsBuilder<TDbContext>();
             builder.UseInMemoryDatabase(nameof(TDbContext));
+            return builder.Options;
+        }
+
+
+        private static DbContextOptions<TDbContext> GetSqlLiteInMemoryDbOptions<TDbContext>() where TDbContext : DbContext
+        {
+            var builder = new DbContextOptionsBuilder<TDbContext>();
+
+            var connection = new SqliteConnection("DataSource=:memory:");
+
+            connection.Open();
+
+            builder.UseSqlite(connection);
+
             return builder.Options;
         }
     }
